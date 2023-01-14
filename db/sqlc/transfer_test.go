cb71853b3ac2f2183cffdd9210f665132e9d9c15
luckyluKINGG <113429970+luckyluKINGG@users.noreply.github.com>
2023-01-14 12:03:58 +0100

    Update transfer_test.go

--- db/sqlc/transfer_test.go
+++ db/sqlc/transfer_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"github.com/techschool/simplebank/util"
+	"github.com/luckyluKINGG/bank_transactions/util"
 )
 
 func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
