c6e31f8e773b6c920f5a9e09ca3dd5c96adda1d5
luckyluKINGG <113429970+luckyluKINGG@users.noreply.github.com>
2023-01-14 12:11:43 +0100

    Update converter.go

--- gapi/converter.go
+++ gapi/converter.go
@@ -1,8 +1,8 @@
 package gapi
 
 import (
-	db "github.com/techschool/simplebank/db/sqlc"
-	"github.com/techschool/simplebank/pb"
+	db "github.com/luckyluKINGG/bank_transactions/db/sqlc"
+	"github.com/luckyluKINGG/bank_transactions/pb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
